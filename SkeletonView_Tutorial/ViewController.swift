0416faad82a2b8e5769164361c20ac9137f9b0a5
prettyturtle <unifconti@gmail.com>
2023-02-18 22:56:14 +0900

    Initial Commit

---
+++ SkeletonView_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkeletonView_Tutorial
+//
+//  Created by yc on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
